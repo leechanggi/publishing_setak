a0acb86263f335ae0381b35d05713a49e36536b7
leechanggi <lxyex1379@naver.com>
2021-01-09 22:08:07 +0900

    formal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,32 @@
+// html - pc || tab_mob
+$(window).on('resize', windeowResizeHandler);
+
+function windeowResizeHandler() {
+    var winWidth = $(this).innerWidth();
+    var scrollWidth = 17
+    if (winWidth >= 1200 - scrollWidth) {
+        $('html').removeClass('tab_mob')
+        $('html').addClass('pc')
+    } else {
+        $('html').removeClass('pc')
+        $('html').addClass('tab_mob')
+    }
+}
+$(window).trigger('resize');
+
+var filter = "win16|win32|win64|mac|macintel";
+if (navigator.platform) {
+    if (filter.indexOf(navigator.platform.toLowerCase()) >= 0) {
+        $(document).ready(function () {
+            var areaWidth = $("#footer").innerWidth();
+            $("#footer").css("width", areaWidth)
+            $(window).resize(function () {
+                location.reload();
+            })
+        })
+    } else {}
+}
+
 //GNB 메뉴//
 $(function () {
     var $firstMenu = $('.gnb > ul > li'),
@@ -21,23 +50,47 @@ $(function () {
         })
 });
 // 전체 메뉴 //
-var menu = document.getElementById("all_menu");
+function openmenu() {
+    var menu = document.getElementById("all_menu");
+    menu.style.top = "0";
+    menu.style.left = "0";
+}
 
 function closemenu() {
-    menu.style.top = "-100vh";
+    var menu = document.getElementById("all_menu");
+    if ($('html').hasClass('pc')) {
+        menu.style.top = "-300vh";
+        menu.style.left = "0";
+    } else {
+        menu.style.left = "-300vw";
+        menu.style.top = "0";
+    }
 }
-
-function openmenu() {
-    menu.style.top = "0";
-}
+$('.all-menu-active').click(function (a) {
+    if ($('html').hasClass('tab_mob')) {
+        $(this).siblings('.all-menu-respon').slideToggle()
+        // $(this).siblings('.all-menu-respon').slideUp();
+        if ($(this).hasClass('on')) {
+            $(this).siblings().removeClass('on');
+            $(this).removeClass('on');
+        } else {
+            $(this).addClass('on').siblings().removeClass('on');
+        }
+    } else {
+        $(this).find('.all-menu-active.on').removeClass('on');
+    }
+    a.preventDefault();
+});
 // 서브 슬라이드 //
 $(function () {
     $('.row2_slide_wrap').bxSlider({
         moveSlides: 1,
+        maxSlides: 2,
         auto: true,
         pager: true,
-        useCSS: false,
-        slideWidth: 600
+        useCSS: true,
+        slideWidth: 600,
+        shrinkItems: true
     });
 });
 // 숫자증가 - row3 //
@@ -159,7 +212,7 @@ $(btn).on('click', function () {
 var menu_login = document.getElementById("login-form");
 
 function closemenu_login() {
-    menu_login.style.right = "-100vw";
+    menu_login.style.right = "-150vw";
 }
 
 function openmenu_login() {
@@ -211,4 +264,40 @@ $(document).ready(function () {
         $('.containerMenu__ul').find('.list.on').removeClass('on');
         $(this).addClass('on');
     })
+})
+//sub1-모바일 슬라이드
+if ($('html').hasClass('tab_mob')) {
+    $('.top-list-wrap').bxSlider({
+        moveSlides: 1,
+        maxSlides: 4,
+        auto: true,
+        pager: false,
+        useCSS: true,
+        slideWidth: 300,
+        shrinkItems: false,
+        slideMargin: 10,
+        controls: false,
+        speed: 900
+    });
+}
+//sub3-모두동의
+$(document).ready(function () {
+    $('.allSign').click(function () {
+        $('.step1').find('.step1Sub').attr('checked', true);
+    })
+})
+//sub3-아이디 중복확인 모달창
+$(document).ready(function () {
+    $('.iddpOpen').click(function () {
+        $('.id-dp-wrap').addClass('on');
+    });
+})
+$(document).ready(function () {
+    $('.iddpClose').click(function () {
+        $('#container').find('.id-dp-wrap.on').removeClass('on');
+    });
+});
+//addr-pop
+$(".addr-pop").click(function () {
+    window.open("addr-pop.html", "우편번호 검색", "width = 525, height = 370, left = 100, top = 100, scrollbars = 0, toolbar = 0, menubar = 0");
 })
